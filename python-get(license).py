174cfb9f5f59eee33f52b1ec2eff8ce2fd26c45c
Kenneth Ng <ngguankiat@hotmail.com>
2021-11-29 06:52:16 +0800

    initial commit

---
+++ python-get(license).py
@@ -0,0 +1,22 @@
+import requests
+import json
+from requests import auth
+import urllib3, xmltodict
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+host="192.168.0.100"
+authdata=('admin','cisco123')
+headers = {'Accept': 'application/yang-data+json','Content-Type':'application/yang-data+json'}
+
+def uri(host,parameters):
+        baseUri = "https://"+ host + parameters
+        return baseUri
+
+#Hostname before change
+baseUri=uri(host,"/restconf/data/Cisco-IOS-XE-native:native")
+getdata = requests.get(baseUri, auth=authdata, verify=False)
+data=(xmltodict.parse(getdata.content))
+#print (json.dumps(data, indent=2))
+print ("ios :" + data["native"]["version"])
+print ("Cisco Module:" + data["native"]["license"]["udi"]["pid"])
+print ("Serial Number:" + data["native"]["license"]["udi"]["sn"])
